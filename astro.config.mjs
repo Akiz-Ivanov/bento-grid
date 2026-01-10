3293c9df82e4ee1726362ac856df0a513d255d69
Akiz-Ivanov <ivanovicandreja@gmail.com>
2026-01-10 17:03:01 +0100

    add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bento-grid.netlify.app',
+  site: 'https://app.netlify.com/projects/capable-sprite-a5faec/deploys/',
 });
